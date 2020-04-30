46e29f52f9307f3c75fff560597b8a2a1b3bf41c
Josh Gillette <joshtg.007@gmail.com>
2020-04-30 09:27:03 -0400

    Allow multiple stocks for query request

--- services/query_service.py
+++ services/query_service.py
@@ -11,23 +11,24 @@ class QueryService:
         self.config = config
 
         self.queryRequest = self.config.get('query', QueryRequest)
-        self.fileService = FileService('data/{}_{}_to_{}_{}.txt'.format(self.queryRequest.symbol,
-                                                                        self.queryRequest.start.strftime(self.config.get('dateFormat')),
-                                                                        self.queryRequest.end.strftime(self.config.get('dateFormat')),
-                                                                        self.queryRequest.interval))
 
 
     def start(self):
-        results = self.performQuery()
+        for symbol in self.queryRequest.symbols:
+            results = self.performQuery(symbol)
 
-        resultsStr = ''
-        for row in results:
-            resultsStr += '{}\n'.format(row)
+            resultsStr = ''
+            for row in results:
+                resultsStr += '{}\n'.format(row)
 
-        self.fileService.write(resultsStr)
+            fileService = FileService('data/{}_{}_to_{}_{}.txt'.format(symbol,
+                                                                       self.queryRequest.start.strftime(self.config.get('dateFormat')),
+                                                                       self.queryRequest.end.strftime(self.config.get('dateFormat')),
+                                                                       self.queryRequest.interval))
+            fileService.write(resultsStr)
 
 
-    def performQuery(self):
+    def performQuery(self, symbol):
         results = []
         start = self.queryRequest.start
         end = self.queryRequest.end
@@ -37,7 +38,7 @@ class QueryService:
                 end = start + datetime.timedelta(days=7)
 
             # Get data
-            data = yf.Ticker(self.queryRequest.symbol).history(start=start, end=end, interval=self.queryRequest.interval)
+            data = yf.Ticker(symbol).history(start=start, end=end, interval=self.queryRequest.interval)
             dateTimes = data.index.values
             for rowIndex in range(len(data)):
                 timeStamp = pandas.to_datetime(str(dateTimes[rowIndex]))
--- structures/query_request.py
+++ structures/query_request.py
@@ -4,14 +4,14 @@ from datetime import datetime
 class QueryRequest:
 
     def __init__(self):
-        self.symbol = None
+        self.symbols = None
         self.symbol = None
         self.end = None
         self.interval = None
 
 
     def deserialize(self, config):
-        self.symbol = config.get('symbol')
+        self.symbols = config.get('symbols')
         self.start = datetime.strptime(config.get('start'), '%Y-%m-%d')
         self.end = datetime.strptime(config.get('end'), '%Y-%m-%d')
         self.interval = config.get('interval')
