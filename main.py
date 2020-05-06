5cc1b4c6c54887f5381a9569098a39ace7f92456
Josh Gillette <joshtg.007@gmail.com>
2020-05-05 22:27:09 -0400

    Set working directory in main

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import sys
 sys.path.append('interfaces/')
 sys.path.append('services/')
 sys.path.append('objects/')
+import os
 from stocking import Stocking
 
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
     Stocking(sys.argv[1]).start()
