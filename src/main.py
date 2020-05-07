976942d2056d82bb3450d512e45869da50737c46
Josh Gillette <joshtg.007@gmail.com>
2020-05-07 10:30:36 -0400

    One more restructure

--- src/main.py
+++ src/main.py
@@ -8,5 +8,6 @@ from stocking import Stocking
 if __name__ == "__main__":
     # Set stocking/ as working directory
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    os.chdir('..')
 
     Stocking(sys.argv[1]).start()
