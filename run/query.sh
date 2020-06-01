2694dd7a1db0a6ab657ecc307ce3d44833324bec
Josh Gillette <joshtg.007@gmail.com>
2020-06-01 11:20:26 -0400

    Update query run script with new args

--- run/query.sh
+++ run/query.sh
@@ -1,8 +1,8 @@
 # Set working directory to stocking/
 cd ~/stocking/
 
-# Run stocking with query flag
-python3 src/main.py config/main_config.json 0
+# Run stocking query service
+python3 src/main.py query run/config/query_all_1d.json
 
 # Commit and push changes to origin
 git add data/stock_data/
