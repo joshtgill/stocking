ec1008afad17da3b1b153fae838997d709ed5e59
Josh Gillette <joshtg.007@gmail.com>
2020-06-11 14:12:46 -0400

    Update run script

--- exe/run.sh
+++ exe/run.sh
@@ -4,3 +4,7 @@ cd ~/stocking/
 # Run stocking's query service
 python3 src/main.py exe/config.json
 
+# Commit updated database files
+git add data/
+git commit -m "Update stock databases"
+git push
