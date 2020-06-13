0011e08aaaa2c26222cde86fb7d4c6d959d7617d
Josh Gillette <joshtg.007@gmail.com>
2020-06-12 22:37:46 -0400

    Insert/replace based on indexed timestamp

--- src/common/data_interface.py
+++ src/common/data_interface.py
@@ -9,25 +9,35 @@ class DataInterface:
 
 
     def insert(self, tableName, tableHeader, data):
-        self.createTable(tableName, tableHeader)
+        self.createTable(tableName, tableHeader, 0)
 
-        self.cursor.executemany("INSERT INTO '{}' VALUES (?, ?, ?, ?, ?)".format(tableName), data)
+        self.cursor.executemany("INSERT OR REPLACE INTO '{}' (timestamp, open, high, low, close) VALUES (?, ?, ?, ?, ?)".format(tableName), data)
 
         self.database.commit()
 
 
-    def createTable(self, name, header):
+    def createTable(self, name, header, index=-1):
         self.cursor.execute("CREATE TABLE IF NOT EXISTS '{}' {}".format(name, header))
+
+        if index != -1:
+            self.cursor.execute("CREATE UNIQUE INDEX IF NOT EXISTS idx_timestamp on '{}' (timestamp)".format(name))
+
+
+    def tableExists(self, name):
+        self.cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{}'".format(name))
+
+        return self.cursor.fetchone()[0] != 0
 
 
     def select(self, tableName, numLastEntries=0):
-        data = []
+        if not self.tableExists(tableName):
+            return []
 
         if not numLastEntries:
             self.cursor.execute("SELECT * FROM '{}'".format(tableName))
         else:
-            self.cursor.execute('''IF (EXISTS SELECT * FROM
+            self.cursor.execute('''SELECT * FROM
                                    (SELECT * FROM '{}' ORDER BY timestamp DESC LIMIT {})
-                                   ORDER BY timestamp ASC)'''.format(tableName, numLastEntries))
+                                   ORDER BY timestamp ASC'''.format(tableName, numLastEntries))
 
         return self.cursor.fetchall()
