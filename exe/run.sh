672762115fd59d100add932e6c6825ca83eff50e
Josh Gillette <joshtg.007@gmail.com>
2020-06-13 17:47:03 -0400

    Remove unnecessary zipping

--- exe/run.sh
+++ exe/run.sh
@@ -3,7 +3,3 @@ cd ~/stocking/
 
 # Run stocking's query service
 python3 src/main.py exe/config.json
-
-# Zip stock data files for easy transfer
-rm -f data/stock_data.zip
-zip data/stock_data.zip data/*
