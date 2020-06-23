0547c2af4e6ac817c4313bf76d046e74ae8fe4c4
Josh Gillette <joshtg.007@gmail.com>
2020-06-22 21:25:04 -0400

    Standardize config naming

--- exe/run.sh
+++ exe/run.sh
@@ -2,10 +2,10 @@
 cd ~/stocking/
 
 # Run stocking's query service
-python3 src/main.py exe/config.json
+python3 src/main.py exe/query_config.json
 
 # Notify Josh with results
 /usr/sbin/sendmail joshtg.007@gmail.com < out/email.txt
 
-# Remove log file in email
+# Remove log file (already in email)
 rm *.log
