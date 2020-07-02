fc91c663017c77ffbb2b5d307204d7a4cfb05713
Josh Gillette <joshtg.007@gmail.com>
2020-07-02 09:40:49 -0400

    Rearrange config files

--- exe/run.sh
+++ exe/run.sh
@@ -5,7 +5,7 @@ cd ~/stocking/
 git pull
 
 # Run stocking's query service
-python3 src/main.py exe/query_config.json
+python3 src/main.py exe/config/query_config.json
 
 # Notify Josh with results
 /usr/sbin/sendmail joshtg.007@gmail.com < out/email.txt
