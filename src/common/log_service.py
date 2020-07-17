226c9ddfc3f608e1ed69f300d1ed3c3b6b670b45
Josh Gillette <joshtg.007@gmail.com>
2020-07-17 19:56:54 -0400

    Enhance LogService to use OrderedDict

--- src/common/log_service.py
+++ src/common/log_service.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from datetime import datetime
 
 
@@ -6,21 +7,21 @@ class LogService:
     def __init__(self, fileInterface, configInterface):
         self.fileInterface = fileInterface
         self.configInterface = configInterface
-        self.taskDir = []  # [(name, start datetime)]
+        self.taskDir = OrderedDict()  # {name: start time}
 
         self.fileInterface.wipe(self.configInterface.settingsGet('stockingLogPath'))
 
 
     def track(self, taskName):
-        self.taskDir.append((taskName, datetime.now()))
+        self.taskDir.update({taskName: datetime.now()})
         self.log('Started', 'INFO')
 
 
     def untrack(self, taskName):
-        self.log('Completed in {}'.format(datetime.now() - self.taskDir[-1][1]), 'STAT')
-        del self.taskDir[-1]
+        self.log('Completed in {}'.format(datetime.now() - list(self.taskDir.values())[-1]), 'STAT')
+        self.taskDir.pop(taskName, None)
 
 
     def log(self, message, logType='INFO'):
         self.fileInterface.write(self.configInterface.settingsGet('stockingLogPath'),
-                                 '[{}] ({}::{}): {}\n'.format(datetime.now(), logType, self.taskDir[-1][0], message))
+                                 '[{}] ({}::{}): {}\n'.format(datetime.now(), logType, list(self.taskDir.keys())[-1], message))
