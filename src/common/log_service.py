6a939f34374d78c0405bdf0fac7710ae93523066
Josh Gillette <joshtg.007@gmail.com>
2020-07-17 19:38:04 -0400

    Refactor LogService members

--- src/common/log_service.py
+++ src/common/log_service.py
@@ -6,21 +6,21 @@ class LogService:
     def __init__(self, fileInterface, configInterface):
         self.fileInterface = fileInterface
         self.configInterface = configInterface
-        self.serviceDir = []  # [(name, start datetime)]
+        self.taskDir = []  # [(name, start datetime)]
 
         self.fileInterface.wipe(self.configInterface.settingsGet('stockingLogPath'))
 
 
-    def track(self, service):
-        self.serviceDir.append((service, datetime.now()))
+    def track(self, taskName):
+        self.taskDir.append((taskName, datetime.now()))
         self.log('Started', 'INFO')
 
 
-    def untrack(self, service):
-        self.log('Completed in {}'.format(datetime.now() - self.serviceDir[-1][1]), 'STAT')
-        del self.serviceDir[-1]
+    def untrack(self, taskName):
+        self.log('Completed in {}'.format(datetime.now() - self.taskDir[-1][1]), 'STAT')
+        del self.taskDir[-1]
 
 
     def log(self, message, logType='INFO'):
         self.fileInterface.write(self.configInterface.settingsGet('stockingLogPath'),
-                                 '[{}] ({}::{}): {}\n'.format(datetime.now(), logType, self.serviceDir[-1][0], message))
+                                 '[{}] ({}::{}): {}\n'.format(datetime.now(), logType, self.taskDir[-1][0], message))
