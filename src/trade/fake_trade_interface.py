c7d3cf344de14555b7b041fd67ba87bf0f587b9f
Josh Gillette <joshtg.007@gmail.com>
2020-07-27 12:42:08 -0400

    Add dummy buy process in TradeInterface

--- src/trade/fake_trade_interface.py
+++ src/trade/fake_trade_interface.py
@@ -12,4 +12,10 @@ class FakeTradeInterface:
     def buy(self, symbol, numShares):
         lastClosePrice = self.stockDataInterface.load(symbol, numLastRows=1)[0][4]
 
-        self.logService.log('Buying {} shares of {} at ${} a share'.format(numShares, symbol, lastClosePrice))
+        numOwnedShares = 0
+        if symbol in self.dataInterface.bank:
+            numOwnedShares = self.dataInterface.bank.get(symbol)
+        self.dataInterface.bank.update({symbol: numShares + numOwnedShares})
+        self.dataInterface.bankSave()
+
+        self.logService.log('Bought {} shares of {} at ${} a share'.format(numShares, symbol, lastClosePrice))
