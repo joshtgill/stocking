46b645d853aac5e837df1c144b66e7b57215c68a
Josh Gillette <joshtgill@xps.lan>
2020-07-27 14:26:22 -0400

    Create out/ directory if needed

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import sys
-sys.stdout = open('out/output.txt', 'w+')
 import os
 from stocking import Stocking
 
@@ -8,6 +7,11 @@ if __name__ == "__main__":
     # Set stocking/ as working directory
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
     os.chdir('..')
+
+    # Direct all output to file
+    if not os.path.exists('out/'):
+        os.mkdir('out/')
+    sys.stdout = open('out/output.txt', 'w+')
 
     # Start Stocking with config
     Stocking(sys.argv[1], 'exe/settings.json').go()
