1b6ae5dd9beb58324554381c4c75364456bf5b45
Josh Gillette <joshtg.007@gmail.com>
2020-08-03 19:56:34 -0400

    Remove logic from MinuteAnalyzeService and StatisticsService

--- src/process/analyze/minute_analyze_service.py
+++ src/process/analyze/minute_analyze_service.py
@@ -15,20 +15,4 @@ class MinuteAnalyzeService:
     def go(self):
         self.logService.track('MINUTE ANALYZE')
 
-        for symbol in self.symbols:
-            approved = self.vetStock(symbol)
-            break
-
         self.logService.untrack('MINUTE ANALYZE')
-
-
-    def vetStock(self, symbol):
-        self.stockDataInterface.load('1m', symbol, start=self.start, end=self.end)
-
-        minuteDataItem = self.stockDataInterface.peek()
-        while minuteDataItem:
-            print(minuteDataItem)
-
-            minuteDataItem = self.stockDataInterface.next()
-
-        return True
--- src/process/statistics/statistics_service.py
+++ src/process/statistics/statistics_service.py
@@ -11,29 +11,4 @@ class StatisticsService:
     def go(self):
         self.logService.track('STATISTICS')
 
-        for symbol in self.symbols:
-            self.observeDayGrowthSpike(symbol)
-
         self.logService.untrack('STATISTICS')
-
-
-    def observeDayGrowthSpike(self, symbol):
-        self.stockDataInterface.load('1d', symbol, self.start, self.end)
-
-        if not self.stockDataInterface.peek():
-            return 0
-
-        spikeFound = False
-        lastStockPrice = self.stockDataInterface.peek()[4]
-        while self.stockDataInterface.next():
-            stockPrice = self.stockDataInterface.peek()[4]
-
-            percentGrowth = ((stockPrice - lastStockPrice) / lastStockPrice) * 100
-            if spikeFound:
-                print(percentGrowth)
-                spikeFound = False
-            else:
-                if percentGrowth > 40:
-                    spikeFound = True
-
-            lastStockPrice = stockPrice
