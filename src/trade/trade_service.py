7bdec6fe404b4c37ab8c3df0e06dacc7d65e16a0
Josh Gillette <joshtg.007@gmail.com>
2020-08-08 21:59:09 -0400

    Remove logic from TradeService

--- src/trade/trade_service.py
+++ src/trade/trade_service.py
@@ -1,28 +1,13 @@
-import json
-from trade.fake_trade_interface import FakeTradeInterface
-
-
 class TradeService:
 
     def __init__(self, dataInterface, logService, fileInterface, stockDataInterface):
         self.dataInterface = dataInterface
         self.logService = logService
         self.fileInterface = fileInterface
-        self.tradeInterface = FakeTradeInterface(dataInterface, logService, stockDataInterface)
         self.stocksToTrade = []
 
 
     def go(self):
         self.logService.track('TRADE')
 
-        self.buyStocks()
-
         self.logService.untrack('TRADE')
-
-
-    def buyStocks(self):
-        for symbol in self.stocksToTrade:
-            self.tradeInterface.buy(symbol, 5)
-
-        self.dataInterface.trades.update({'JOSH': 'GILLETTE'})
-        self.dataInterface.tradesSave()
