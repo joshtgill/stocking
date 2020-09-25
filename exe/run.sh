3e98e2d894a4e7a7f7ad7453ef739e9d9e235438
Josh Gillette <joshtg.007@gmail.com>
2020-09-25 17:42:09 -0400

    Reorganize exe/ directory

--- exe/run.sh
+++ exe/run.sh
@@ -2,7 +2,7 @@
 cd ~/stocking/
 
 # Run stocking's query service
-python3 src/main.py exe/config/release/query_config.json
+python3 src/main.py exe/release/query_config.json
 
 # Save a backup of data in a separate location
 cp data/*.db ~/backup/
